d2ca785e51e7a9c49cec8255226fee9ffeab07c7
Mike Yin <yinjitong@hotmail.com>
2016-07-04 17:04:11 -0400

    adding files

---
+++ test/testing file.py
@@ -0,0 +1,19 @@
+
+import serial
+
+ser = serial.Serial(port='ttyUSB10',baudrate=9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=0)
+
+print("connected to: " + ser.portstr)
+
+#this will store the line
+line = []
+
+while True:
+    for c in ser.read():
+        line.append(c)
+        if c == '\n':
+            print("Line: " + line)
+            line = []
+            break
+
+
