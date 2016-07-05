4a1ded718a1d4de674619f72994123c9bf1dc4e8
mikeyin97 <yinjitong@hotmail.com>
2016-07-05 18:10:21 -0400

    adding clustering algorithm to data collection

--- test/livedata.py
+++ test/livedata.py
@@ -7,6 +7,8 @@ import csv
 import os
 import random
 import datetime
+import numpy as np
+import skfuzzy as fuzz
 os.path.abspath("C:\\Users\\Michael\\Documents\\GitHub\\EMG\\test\\csvfiles")
 
 #getting serial data
@@ -19,6 +21,7 @@ print("connected to: " + ser.portstr)
 line = []           #storing the numbers until it reaches a space
 index = 0           #for some reason the first value is buggy, use this so circumvent
 all_data = [["logrms1","logrms2"]]
+colors = ['g', 'm', 'b', 'r', 'c', 'y', 'k', 'Brown', 'ForestGreen']
 #test variables
 #vallist = []        
 #val_a = []
@@ -33,10 +36,10 @@ time1 = time.time()
 time2 = time.time()
 
 
-#main loop
+#MAIN LOOP
 
 #while True:
-while time2 - time1 < 5:    
+while time2 - time1 < 30:    
     for c in ser.readline():
         if not(c == 13):
             line.append(chr(c)) 
@@ -56,8 +59,8 @@ while time2 - time1 < 5:
                 x = float(a[0])
                 y = float(a[1])
                 all_data.append([x,y])
-                plt.scatter(x,y)
-                plt.pause(0.0001)
+                plt.scatter(x,y,s=10, color = "y")
+                plt.pause(0.00000001)
                 time2 = time.time()
             index += 1
             line = []
@@ -76,6 +79,61 @@ with open('C:\\Users\\Michael\\Documents\\GitHub\\EMG\\test\\csvfiles\\test' + n
     for i in all_data:
         writer.writerow(i)
 
+print("file saved as: " + 'C:\\Users\\Michael\\Documents\\GitHub\\EMG\\test\\csvfiles\\test' + str(num) + '.csv')
+
+#make a nested list, first list is x values seperated by double space, second list is y values
+
+x_val = []
+y_val = []
+for i in all_data:
+    x_val.append(i[0])
+    y_val.append(i[1])
+alldata = [x_val[2:len(x_val)], y_val[2:len(y_val)]]
+x_val = alldata[0];
+y_val = alldata[1];
+alldata = np.asarray(alldata)
+#print(alldata)
+
+#running fuzzy c-means algorithm (second num is # clusters)
+cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+    alldata, 4, 2, error=0.0005, maxiter=10000, init=None, seed=None)
+cluster_membership = np.argmax(u, axis=0)
+print(cluster_membership)
+#change depending on number clusters
+for j in range(4):
+    for i in range(len(cluster_membership)):
+        if cluster_membership[i] == j:
+            plt.plot(x_val[i], y_val[i], '.', color = colors[j])
+    plt.plot(cntr[j][0], cntr[j][1], colors[j]+"s")
+
+#SECONDARY LOOP
+#continuing after saved file (indefinitely atm)
+
+while True:    
+    for c in ser.readline():
+        if not(c == 13):
+            line.append(chr(c)) 
+        elif (c == 13):
+            a = ("".join(str(x) for x in line))
+            a = a.replace("\n", ",")
+            a = a.split(",")
+            a = ([x for x in a if x])
+            if index!=0:
+                print(a)
+            #if index%2 == 0:
+            #    val_a.append(a[index])
+            #elif index%2 == 1:
+            #    print(val_b)
+            #    val_b.append(a[index])
+                
+                x = float(a[0])
+                y = float(a[1])
+                all_data.append([x,y])
+                plt.scatter(x,y,s=10, color = "y")
+                plt.pause(0.00000001)
+                time2 = time.time()
+            index += 1
+            line = []
 
 
 #with open('test1.csv', 'wb') as testfile:
--- test/livedata3D.py
+++ test/livedata3D.py
@@ -4,12 +4,16 @@ import serial
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+ax = fig.add_subplot(111,projection='3d')
+#fig = plt.figure()
+#ax = plt.axes(projection='3d')
 import time
 import random
 import csv
 import os
 os.path.abspath("C:\\Users\\Michael\\Documents\\GitHub\\EMG\\test\\csvfiles")
+import numpy as np
+import skfuzzy as fuzz
 import datetime
 
 #getting serial data
@@ -22,6 +26,7 @@ print("connected to: " + ser.portstr)
 line = []           #storing the numbers until it reaches a space
 index = 0           #for some reason the first value is buggy, use this so circumvent
 all_data = [["logrms1","logrms2","logrms3"]]
+colors = ['g', 'm', 'b', 'r', 'c', 'y', 'k', 'Brown', 'ForestGreen']
 #test variables
 #vallist = []        
 #val_a = []
@@ -35,10 +40,10 @@ time1 = time.time()
 time2 = time.time()
 
 
-#main loop
+#MAIN LOOP
 
 #while True:
-while time2 - time1 < 3:    
+while time2 - time1 < 120:    
     for c in ser.readline():
         if not(c == 13):
             line.append(chr(c)) 
@@ -47,16 +52,17 @@ while time2 - time1 < 3:
             a = a.replace("\n", ",")
             a = a.split(",")
             a = ([x for x in a if x])
-            print(a)
             if index!=0:
                 x = float(a[0])
                 y = float(a[1])
                 #z needs to be initialized to something (probably a[2] but needs testing)
                 z = random.randint(0,10)
                 all_data.append([x,y,z])
-                ax.scatter(x,y,z)
+                print([x,y,z])
+                ax.scatter(x,y,z, c='y', s=10, marker = "o")
+                #ax.plot(x,y,z,'-b')
                 #needs delay or else crashes
-                plt.pause(0.0001)
+                plt.pause(0.00000001)
                 
                 time2 = time.time()
             
@@ -76,3 +82,62 @@ with open('C:\\Users\\Michael\\Documents\\GitHub\\EMG\\test\\csvfiles\\test' + s
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for i in all_data:
         writer.writerow(i)
+
+print("file saved as: " + 'C:\\Users\\Michael\\Documents\\GitHub\\EMG\\test\\csvfiles\\test' + str(num) + '.csv')
+
+
+#make a nested list, first list is x values seperated by double space, second list is y values
+x_val = []
+y_val = []
+z_val = []
+for i in all_data:
+    x_val.append(i[0])
+    y_val.append(i[1])
+    z_val.append(i[2])
+alldata = [x_val[2:len(x_val)], y_val[2:len(y_val)], z_val[2:len(z_val)]] #don't want the titles
+x_val = alldata[0];
+y_val = alldata[1];
+z_val = alldata[2];
+alldata = np.asarray(alldata)
+#print(alldata)
+
+#running fuzzy c-means algorithm (second num is # clusters)
+cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+    alldata, 4, 2, error=0.0005, maxiter=10000, init=None, seed=None)
+cluster_membership = np.argmax(u, axis=0)
+print(cluster_membership)
+#change depending on number clusters
+for j in range(4):
+    for i in range(len(cluster_membership)):
+        if cluster_membership[i] == j:
+            ax.scatter(x_val[i], y_val[i], z_val[i], c = colors[j],s = 20, marker = "o")
+    ax.scatter(cntr[j][0], cntr[j][1], cntr[j][2], c = colors[j], marker = "s", s = 50)
+
+
+#SECONDARY LOOP
+#continuing on after data has been saved (indefinitely atm)
+
+while True:    
+    for c in ser.readline():
+        if not(c == 13):
+            line.append(chr(c)) 
+        elif (c == 13):
+            a = ("".join(str(x) for x in line))
+            a = a.replace("\n", ",")
+            a = a.split(",")
+            a = ([x for x in a if x])
+            if index!=0:
+                x = float(a[0])
+                y = float(a[1])
+                #z needs to be initialized to something (probably a[2] but needs testing)
+                z = random.randint(0,10)
+                print([x,y,z])
+                all_data.append([x,y,z])
+                ax.scatter(x,y,z, c='y', s=10, marker = "o")
+                #needs delay or else crashes
+                plt.pause(0.00000001)
+                
+                time2 = time.time()
+            
+            index += 1
+            line = []
