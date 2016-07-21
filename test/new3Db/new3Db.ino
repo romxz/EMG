0a00e18382027c62a7f455536e1c86f8cd1897a7
Andrei <andreiromalv+git@gmail.com>
2016-07-21 17:27:06 -0400

    changes to the wave generator: Now each channel is more singular, can touch zero, and has adjustable crosstalk from other channels

--- test/new3Db/new3Db.ino
+++ test/new3Db/new3Db.ino
@@ -37,52 +37,57 @@ long decay_cnt = 0;
 #define AVG_MEM_ROOT 1.0
 
 // Variables for 4 sensor signal manipulation:
-#define AMPLIFY_DIFF 1.0
-#define AMPLIFY_DIFF_RADIUS 0.1
-#define RMS_AVG_AMP_MOD 0.6
+#define NEW_DIFF 1
+#define AMP_DIFF_OLD 1.0
+#define AMP_DIFF_OLD_RADIUS 0.1
+#define RMS_AVG_AMP_MOD 0.5
 
 // Print Modes and Parameters
 #define PRINT_RMS_RAW 0
-#define PRINT_RMS_AVG 0
+#define PRINT_RMS_AVG 1
 #define USE_DECAY_MAX 1
 #define PRINT_RMS_MAX_DECAY 0
 #define PRINT_RMS_DIFF 0
-#define PRINT_RMS_MOD 1
+#define PRINT_RMS_MOD 0
 #define PRINT_RMS_LOG 0
 #define PRINT_NUM_DECIMALS 2
-#define PRINT_DELAY 0.001
+#define PRINT_DELAY 0.005
 #define SEE_WAVEFORM_OLD 1
 const int PRINT_CHANNEL_S[] = {1, 1, 1, 1};
 
 // Fake Input Parameters
 #define FAKE_INPUT true
-const double FAKE_S[] = {20.1, 50.1112, 3.14159, 2.71};
-int counterFakeTime = 0;
-#define SPEED_CONSTANT 0.1
+const double FAKE_BIAS_S[] = {2.1, 3.11, 0.52, 5.71};
+const double FAKE_CROSSTALK_S[] = {0, 0.1, 0.5, 1};
+int cntFakeTime = 0;
+const double SPEED_ACT_MOD[] = {0.05, 0.056, 0.04, 0.045};
+#define SPEED_CNST_HELD 0.08
 #define ANGLE_PHASE 0.7
-long randNoise;
 
 // Log Output parameters
 #define RMS_MOD_MIN_RANGE 0.001 // Keep modulated rms output above this value
 #define LOG_MAX_RANGE 6 // Keep log output below this value
 
 void randWave(double a[]) {
-  randNoise = random(11);
-  double wave1 = randNoise / 10.0;
-  wave1 = (1 + wave1) * sq(sin(SPEED_CONSTANT * counterFakeTime));
-  randNoise = random(11);
-  double wave2 = randNoise / 10.0;
-  wave2 = (1 + wave2) * sq(sin(SPEED_CONSTANT * counterFakeTime + ANGLE_PHASE + wave1 + wave2));
-  randNoise = random(11);
-  double wave3 = randNoise / 10.0;
-  wave3 = wave3 * sq(sin(SPEED_CONSTANT * counterFakeTime + 2 * ANGLE_PHASE * wave2));
-  randNoise = random(11);
-  double wave4 = randNoise / 20.0;
-  wave4 = (1 + wave4) * sq(sin(SPEED_CONSTANT * counterFakeTime * (1 + wave4) + 3 * ANGLE_PHASE * wave2 + wave3));
+  double sqwave_s[NUM_SENSORS];
+  long randNoise;
   for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
-    a[sensorNum] = FAKE_S[sensorNum] * wave1 + FAKE_S[(sensorNum + 1) % NUM_SENSORS] * wave2 + FAKE_S[(sensorNum + 2) % NUM_SENSORS] * wave3 + FAKE_S[(sensorNum + 3) % NUM_SENSORS] * wave4;
+    randNoise = random(11);
+    sqwave_s[sensorNum] = sensorNum * ANGLE_PHASE * (1 + randNoise / 50.0)));
+    randNoise = random(11);
+    sqwave_s[sensorNum] = sq(sin(sqwave_s[sensorNum] + SPEED_CNST_HELD * cntFakeTime * (1 + randNoise / 20.0)));
+    randNoise = random(11);
+    sqwave_s[sensorNum] = FAKE_BIAS_S[sensorNum] * (randNoise / 10.0) * sqwave_s[sensorNum];
+    randNoise = random(11);
+    sqwave_s[sensorNum] = sqwave_s[sensorNum] * sq(sin(SPEED_ACT_MOD[sensorNum] * cntFakeTime * (1 + randNoise / 50) + sensorNum * ANGLE_PHASE * (1 + randNoise / 60.0))));
   }
-  counterFakeTime++;
+  for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
+    a[sensorNum] = sqwave_s[sensorNum];
+    for (int sensorNumOther = 1; sensorNumOther < NUM_SENSORS - 1; sensorNumOther++) {
+      a[sensorNum] = a[sensorNum] + FAKE_CROSSTALK_S[sensorNum] * sqwave_s[(sensorNum + sensorNumOther) % NUM_SENSORS];
+    }
+  }
+  cntFakeTime++;
 }
 
 void setup() {
@@ -201,7 +206,7 @@ void loop() {
       rms_avg_s[sensorNum] = decaying_max[sensorNum];
       decay_rate = DECAY_MAX_CONST +  (DECAY_MAX_LIN + DECAY_MAX_LINLOG * log(decay_cnt + 1)) * decay_cnt + DECAY_MAX_SQ * sq(decay_cnt) +  DECAY_MAX_ROOT * sqrt(decay_cnt) + DECAY_MAX_LOG * log(decay_cnt + 1);
       decay_rate = decay_rate * DECAY_MAX_AMP;
-      decaying_max[sensorNum] = decaying_max[sensorNum] * (1 - min(decay_rate, 1));
+      decaying_max[sensorNum] = decaying_max[sensorNum] * (1.001 - min(decay_rate, 1));
       decaying_max[sensorNum] = max(decaying_max[sensorNum], DECAY_MIN);
     }
     decay_cnt++;
@@ -209,49 +214,66 @@ void loop() {
     if (PRINT_RMS_MAX_DECAY) printArray(decaying_max);
   }
 
-  // signal strength norm
-  double rms_radius_norm = 0.0;
+  // getting and amplifying differences
+  double rms_diff_s[NUM_SENSORS];
+  double rms_radius_norm = 0.0; // signal strength norm
   for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
     rms_radius_norm = rms_radius_norm + sq(rms_avg_s[sensorNum]);
   }
-  rms_radius_norm = sqrt(rms_radius_norm);
-  // signal difference norm
-  double rms_diff_norm = 1.0;
-  for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
-    for (int sensorNumOther = sensorNum + 1; sensorNumOther < NUM_SENSORS; sensorNumOther++) {
-      rms_diff_norm = rms_diff_norm + sq(rms_avg_s[sensorNumOther] - rms_avg_s[sensorNum]);
+  rms_radius_norm = sqrt(rms_radius_norm) / NUM_SENSORS;
+  if (NEW_DIFF) {
+
+    int rank[NUM_SENSORS] = {1, 1, 1, 1};
+    // get rank of signals in terms of size
+    for (int sensorNum = 0; sensorNum < NUM_SENSORS - 1; sensorNum++) {
+      for (int sensorNumOther = sensorNum + 1; sensorNumOther < NUM_SENSORS; sensorNumOther++) {
+        if (rms_avg_s[sensorNum] < rms_avg_s[sensorNumOther]) {
+          rank[sensorNumOther] = rank[sensorNumOther] + 1;
+        } else {
+          rank[sensorNum] = rank[sensorNum] + 1;
+        }
+      }
     }
-  }
-  rms_diff_norm = sqrt(rms_diff_norm);
-  // relative distance of a channel relative to the other's average
-  double rms_diff_s[NUM_SENSORS];
-  for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
-    rms_diff_s[sensorNum] = 4 * rms_avg_s[sensorNum];
-    for (int sensorNumOther = 0; sensorNumOther < NUM_SENSORS; sensorNumOther++) {
-      rms_diff_s[sensorNum] = rms_diff_s[sensorNum] - rms_avg_s[sensorNumOther];
+    for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
+      rms_diff_s[sensorNum] = rms_radius_norm * rms_avg_s[sensorNum] / 40.0;
     }
-  }
-  // ~sign of distance from avg (& avoiding division by zero):
-  for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
-    rms_diff_s[sensorNum] = rms_diff_s[sensorNum] / (abs(rms_diff_s[sensorNum]) + 1.0);
-  }
-  // capture relevant (signed & normalized) radius of distances:
-  double rms_diff_shift_norm = 0.0;
-  for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
-    double square_of_diff_sum = 0.0;
-    for (int sensorNumOtherDummy = 1; sensorNumOtherDummy < NUM_SENSORS; sensorNumOtherDummy++) {
-      int sensorNumOther = sensorNum + sensorNumOtherDummy;
-      if (sensorNumOther >= NUM_SENSORS) sensorNumOther = sensorNumOther - NUM_SENSORS; // correct index overflow
-      square_of_diff_sum = square_of_diff_sum + sq(rms_avg_s[sensorNumOther] - rms_avg_s[sensorNum]);
+  } else {
+    double rms_diff_norm = 1.0; // signal difference norm
+    for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
+      for (int sensorNumOther = sensorNum + 1; sensorNumOther < NUM_SENSORS; sensorNumOther++) {
+        rms_diff_norm = rms_diff_norm + sq(rms_avg_s[sensorNumOther] - rms_avg_s[sensorNum]);
+      }
     }
-    rms_diff_s[sensorNum] = (rms_diff_s[sensorNum] * sqrt(square_of_diff_sum)) / rms_diff_norm;
-    rms_diff_shift_norm = rms_diff_shift_norm + abs(rms_diff_s[sensorNum]);
-  }
-  for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
-    // shift differences relative to norm to make them positive
-    rms_diff_s[sensorNum] = rms_diff_s[sensorNum] + rms_diff_shift_norm;
-    // amplify original averaged signal with this difference, including a factor for when there is more overall sensor activity
-    rms_diff_s[sensorNum] = (1 + rms_radius_norm * AMPLIFY_DIFF_RADIUS) * AMPLIFY_DIFF * rms_diff_s[sensorNum];
+    rms_diff_norm = sqrt(rms_diff_norm);
+    // relative distance of a channel relative to the other's average
+    for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
+      rms_diff_s[sensorNum] = 4 * rms_avg_s[sensorNum];
+      for (int sensorNumOther = 0; sensorNumOther < NUM_SENSORS; sensorNumOther++) {
+        rms_diff_s[sensorNum] = rms_diff_s[sensorNum] - rms_avg_s[sensorNumOther];
+      }
+    }
+    // ~sign of distance from avg (& avoiding division by zero):
+    for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
+      rms_diff_s[sensorNum] = rms_diff_s[sensorNum] / (abs(rms_diff_s[sensorNum]) + 1.0);
+    }
+    // capture relevant (signed & normalized) radius of distances:
+    double rms_diff_shift_norm = 0.0;
+    for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
+      double square_of_diff_sum = 0.0;
+      for (int sensorNumOtherDummy = 1; sensorNumOtherDummy < NUM_SENSORS; sensorNumOtherDummy++) {
+        int sensorNumOther = sensorNum + sensorNumOtherDummy;
+        if (sensorNumOther >= NUM_SENSORS) sensorNumOther = sensorNumOther - NUM_SENSORS; // correct index overflow
+        square_of_diff_sum = square_of_diff_sum + sq(rms_avg_s[sensorNumOther] - rms_avg_s[sensorNum]);
+      }
+      rms_diff_s[sensorNum] = (rms_diff_s[sensorNum] * sqrt(square_of_diff_sum)) / rms_diff_norm;
+      rms_diff_shift_norm = rms_diff_shift_norm + abs(rms_diff_s[sensorNum]);
+    }
+    for (int sensorNum = 0; sensorNum < NUM_SENSORS; sensorNum++) {
+      // shift differences relative to norm to make them positive
+      rms_diff_s[sensorNum] = rms_diff_s[sensorNum] + rms_diff_shift_norm;
+      // amplify original averaged signal with this difference, including a factor for when there is more overall sensor activity
+      rms_diff_s[sensorNum] = (1 + rms_radius_norm * AMP_DIFF_OLD_RADIUS) * AMP_DIFF_OLD * rms_diff_s[sensorNum];
+    }
   }
   // print differences:
   if (PRINT_RMS_DIFF) printArray(rms_diff_s);
