917bf98f01358efa6de12c5198d2acbdc40632ee
mikeyin97 <yinjitong@hotmail.com>
2016-08-03 17:09:17 -0400

    starting chrome file

---
+++ test/extensions/livedata.js
@@ -0,0 +1,32 @@
+chrome.browserAction.onClicked.addListener(function(activeTab){
+  var newURL = "livedata.html";
+  chrome.tabs.create({ url: newURL });
+});
+
+
+$(document).ready(function() {
+    chrome.serial.getDevices(function(devices) {
+        for (var i = 0; i < devices.length; i++) {
+            $('select#portList').append('<option value="' + devices[i].path + '">' + devices[i].path + '</option>');
+        }
+    });
+    $('button#open').click(function() {
+        var clicks = $(this).data('clicks');
+
+        if (!clicks) {
+            var port = $('select#portList').val();
+            chrome.serial.connect(port, {bitrate: 9600}, function(info) {
+                connectionId = info.connectionId;
+                $("button#open").html("Close Port");
+                console.log('Connection opened with id: ' + connectionId + ', Bitrate: ' + info.bitrate);
+            });
+        } else {
+            chrome.serial.disconnect(connectionId, function(result) {
+                $("button#open").html("Open Port");
+                console.log('Connection with id: ' + connectionId + ' closed');
+            });
+        }
+
+        $(this).data("clicks", !clicks);
+    });
+});
